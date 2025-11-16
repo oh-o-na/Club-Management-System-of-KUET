65ac0deb8204ab41ab2a780f0f0555faca6eedfd
oh-o-na <ohonacse@gmail.com>
2025-11-16 17:28:51 +0600

    Refactor admin_utils.h header guard and includes

--- admin_utils.h
+++ admin_utils.h
@@ -1,14 +1,9 @@
 #ifndef ADMIN_UTILS_H
 #define ADMIN_UTILS_H
-
-// Include the new common header
 #include "common_structs.h"
-
-// Prototypes for shared utility functions
 void clearInputBuffer();
 void clearNewline(char *str);
-void printMember(Member m); // Struct definition is now in common_structs.h
-void printEvent(Event e);   // Struct definition is now in common_structs.h
+void printMember(Member m); 
+void printEvent(Event e);
 void showMenu();
-
 #endif
